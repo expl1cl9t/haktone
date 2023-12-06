1e39d19f14b53efa4cb8be74a086ab25b548942f
expl1cl9t <104081409+expl1cl9t@users.noreply.github.com>
2023-12-06 23:08:47 +0300

    final

--- js/changeSeason.js
+++ js/changeSeason.js
@@ -1,74 +1,113 @@
-let picture = $("#seasonImage");
-$("li[class ^= 'b']").click((e) => {
-    let s = $(e.target).attr("season");
-    picture.css("width", "0");
-    console.log(s);
-    switch (s) {
-        case "z":
-            picture.css("width", "0");
-            picture.animate({
-                width: "+=906"
-            }, 2000, () => { });
-            picture.attr("src", "imgsrc/beautiful-scenery-of-the-summit-of-mount-everest-covered-with-snow-under-the-white-clouds.jpg");
-            break;
-        case "l":
-            picture.css("width", "0");
-            picture.animate({
-                width: "+=906"
-            }, 2000, () => { });
-            picture.attr("src", "imgsrc/лето и арбалеты.jpg");
-            break;
-        case "o":
-            picture.css("width", "0");
-            picture.animate({
-                width: "+=906"
-            }, 2000, () => { });
-            picture.attr("src", "imgsrc/beautiful-shot-of-mountains-under-a-cloudy-sky-at-sunset.jpg");
-            break;
-        case "v":
-            picture.css("width", "0");
-            picture.animate({
-                width: "+=906"
-            }, 2000, () => { });
-            picture.attr("src", "imgsrc/aerial-photography-of-houses-on-green-field-viewing-mountain-under-white-and-gray-sky-during-daytime_400718-49.jpg");
-            break;
-        default:
-            break;
-    }
-});
 
 
+// $(document).ready(function () {
+//     $(".by_order_menu_elem2").mouseenter(() => {
+//         console.log(1);
+//         if (Number($(".by_order_menu_elem2").css("z-index")) > Number($(".by_order_menu_elem1").css("z-index"))) {
+//             return;
+//         }
+//         let el = Number($(".by_order_menu_elem2").css("z-index"));
+//         console.log(el);
+//         el += 1;
+//         $(".by_order_menu_elem2").css("z-index", el);
+//         $(this).css("background-color", "white");
+//     });
+//
+//     $(".by_order_menu_elem1").mouseenter(() => {
+//         if (Number($(".by_order_menu_elem2").css("z-index")) < Number($(".by_order_menu_elem1").css("z-index"))) {
+//             return;
+//         }
+//         let el = Number($(".by_order_menu_elem1").css("z-index"));
+//         el += 1;
+//         $(".by_order_menu_elem1").css("z-index", el);
+//         $(this).css("background-color", "white");
+//     });
+// });
 
-//смена z индекса
-$(".by_order_menu_elem2").click(() => {
-    console.log(1);
-    if(Number($(".by_order_menu_elem2").css("z-index")) > Number($(".by_order_menu_elem1").css("z-index"))){
-        return;
-    }
-    let el = Number($(".by_order_menu_elem2").css("z-index"));
-    console.log(el);
-    el += 1;
-    $(".by_order_menu_elem2").css("z-index",el);
-    $(this).css("background-color","white");
+$(document).ready(function () {
+    $(".by_order_menu_elem2").mouseenter(function() {
+        if (Number($(this).css("z-index")) > Number($(".by_order_menu_elem1").css("z-index"))) {
+            return;
+        }
+        $(this).css({ "z-index": "+=1", backgroundColor: "white" }).animate({ opacity: 1 }, "slow");
+        $(".by_order_menu_elem1").animate({ opacity: 0.7 }, "slow");
+    });
+
+    $(".by_order_menu_elem1").mouseenter(function() {
+        if (Number($(this).css("z-index")) < Number($(".by_order_menu_elem2").css("z-index"))) {
+            return;
+        }
+        $(this).css({ "z-index": "+=1", backgroundColor: "white" }).animate({ opacity: 1 }, "slow");
+        $(".by_order_menu_elem2").animate({ opacity: 0.7 }, "slow");
+    });
 });
-$(".by_order_menu_elem1").click(() => {
-    if(Number($(".by_order_menu_elem2").css("z-index")) < Number($(".by_order_menu_elem1").css("z-index"))){
-        return;
-    }
-    let el = Number($(".by_order_menu_elem1").css("z-index"));
-    el += 1;
-    $(".by_order_menu_elem1").css("z-index",el);
-    $(this).css("background-color","white");
+
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('a[href^="#"]');
+
+    links.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                const offsetTop = targetElement.offsetTop;
+                window.scroll({
+                    top: offsetTop,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
 });
+
+// let images = ["imgsrc/natural-landscape-with-sunrise.jpg","imgsrc/tree-and-mountain-landscape_1048-9294.jpg","imgsrc/view-on-empty-icelandic-road-from-inside-car.jpg"];
+// let ind = 0;
+//
+// function changeImage() {
+//     $("img[class='border']").attr("src",`${images[ind]}`);
+//     $("img[class='border']").animate({ opacity: 1 }, 2000, function() {
+//
+//         setTimeout(changeImage, 1000);
+//     });
+//
+//     ind++;
+//     if (ind >= images.length) {
+//         ind = 0;
+//     }
+// }
+//
+// changeImage();
+
+
 let images = ["imgsrc/natural-landscape-with-sunrise.jpg","imgsrc/tree-and-mountain-landscape_1048-9294.jpg","imgsrc/view-on-empty-icelandic-road-from-inside-car.jpg"];
 let ind = 0;
-setInterval(() => {
-    $("img[class='border']").css("opacity","0");
-    $("img[class='border']").animate({
-        opacity: "+=1"
-    },1000,() => {$("img[class='border']").attr("src",`${images[ind]}`)});
-    ind++;
-    if(ind == 2){
-        ind = 0;
-    }
-}, 5000);
+
+function changeImage() {
+$("img[class='border']").fadeOut(1000, function() {
+$(this).attr("src", images[ind]);
+$(this).fadeIn(1000);
+});
+ind++;
+if (ind >= images.length) {
+    ind = 0;
+}
+
+setTimeout(changeImage, 3000);
+}
+
+changeImage();
+
+
+const infoButton = document.getElementById('infoButton');
+const amazingNearPhoto = document.querySelector('.blur');
+
+infoButton.addEventListener('mouseover', () => {
+    amazingNearPhoto.classList.add('blur-effect');
+});
+
+infoButton.addEventListener('mouseout', () => {
+    amazingNearPhoto.classList.remove('blur-effect');
+});
--- js/fadeHeader.js
+++ js/fadeHeader.js
@@ -1,7 +1,7 @@
 let header = $(".header");
 let lastScroll = 0;
 $(window).scroll(() => {
-    header.css("opacity",1 - $(this).scrollTop() / 500);
+    header.css("opacity",1 - $(this).scrollTop() / 400);
     if(header.css("opacity") == 0){
         header.css("display","none");
     }
